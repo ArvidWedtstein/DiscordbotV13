03833ebd72151a6ba4886646fa09db0dd89ec896
ArvidW <71834553+ArvidWedtstein@users.noreply.github.com>
2022-04-22 14:11:45 +0200

    brawlhalla figses

--- src/Functions/brawlhalla.ts
+++ src/Functions/brawlhalla.ts
@@ -1,105 +1,193 @@
-let twitchloop = true;
 import Client from '../Client';
-import TwitchAPI from 'node-twitch'
-import { MessageButton, MessageEmbed } from 'discord.js';
+import axios from 'axios';
+import { MessageAttachment, MessageButton, MessageEmbed } from 'discord.js';
 import profileSchema from '../schemas/profileSchema';
 import moment from 'moment';
 import gradient from 'gradient-string';
 export const brawlhalla = (async (client: Client) => {
+  
+  interface Stream {
+    id: string;
+    start_time: string;
+    end_time: string;
+    title: string;
+    canceled_until: string;
+    category: { id: string; name: string };
+    is_recurring: boolean;
+  }
+  interface TwitchUser {
+    id: string;
+    login: string;
+    display_name: string;
+    type: string;
+    broadcaster_type: string;
+    description: string;
+    profile_image_url: string;
+    offline_image_url: string;
+    view_count: number;
+    created_at: string;
+  }
+
   let lastStream: any = {
     id: "",
     started_at: ""
   }
-  const twitch = new TwitchAPI({
-    client_id: process.env.TWITCH_CLIENT_ID || '',
-    client_secret: process.env.TWITCH_CLIENT_SECRET || ''
-  })
-  let IsLiveMemory = false
-  
-  
-  const Run = (async () => {
+
+  let streams: Stream[] = [];
+  let twitchuser: TwitchUser;
+
+  let intervalID: any;
+
+  const RunDaily = (async () => {
+    var user = 'brawlhalla'
     try {
-      const date = new Date()
-      let hours = date.getHours()
-
-      await twitch.getStreams({ channel: "brawlhalla" }).then(async data => {
-        const r = data.data[0];
-        if (r === undefined) return IsLiveMemory = false
-        if (r.id === lastStream.id) return
-        if (r.type != "live") return IsLiveMemory = false;
-        
-        if (twitchloop) {
-          console.info('Brawlhalla stream found');
-
-          // open('https://www.twitch.tv/brawlhalla');
-          IsLiveMemory = true;
-          twitchloop = false;
-
-          const result = await profileSchema.find({
-            brawlhalla: true
-          })
-
-          lastStream = r;
-
-          var greet: string;
-
-          var greetings = [
-            'Greetings',
-            "Howdy!",
-            "Hello there"
-          ]
-          var greetings1 = [
-            "Good Morning",
-            ...greetings
-          ]
-          var greetings2 = [
-            "Good Afternoon",
-            ...greetings
-          ]
-          var greetings3 = [
-            "Good Evening",
-            ...greetings
-          ]
-
-          if (hours < 12) {
-            greet = greetings1[Math.floor(Math.random()*greetings1.length)];;
-          } else if (hours >= 12 && hours <= 17) {
-            greet = greetings2[Math.floor(Math.random()*greetings2.length)];
-          } else if (hours >= 17 && hours <= 24) {
-            greet = greetings3[Math.floor(Math.random()*greetings3.length)];
-          }
-            
-          result.forEach(async (user) => {
-            let guild = client.guilds.cache.find((g) => g.id === user.guildId)
-            if (!guild) return;
-
-            let member = guild.members.cache.find((member) => member.id === user.userId)
-
-            var messages = [
-              `I have the pleasure to inform you that **Brawlhalla** currently is streaming, live on Twitch.`,
-              `Did you know?!? **Brawlhalla** is streaming! YES. You heard right! Brawlhalla is streaming right now on twitch!`,
-              `Hey, just wanted to let you know that **Brawlhalla** is currently streaming. Have a good day 😊`,
-              `Hey there fella, just wanna let ya know that **Brawlhalla** is streaming. Have a nice one.`,
-              `G'day mate, just wanna let ya know **Brawlhalla** is live on this twich thingy.`
-            ]
-
-            var randomMsg = messages[Math.floor(Math.random()*messages.length)];
-            let embed = new MessageEmbed()
-              .setTitle(`${greet}, ${member?.user.username}.`)
-              .setURL('https://www.twitch.tv/brawlhalla')
-              .setDescription(randomMsg)
-              .setThumbnail(r.getThumbnailUrl({width: 1000, height: 1000}))
-              .setFooter({ text: `Sincerely, Memebot`, iconURL: client.user?.displayAvatarURL() })
-              .setTimestamp()
-
-            member?.send({ embeds: [embed]})
-          })
+      axios.get(`https://api.twitch.tv/helix/users?login=${user}`, {
+        headers: {
+          'Authorization': `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
+          'Client-ID': process.env.TWITCH_CLIENT_ID || ''
         }
+      }).then((res) => {
+        twitchuser = res.data.data[0];
+        axios.get(`https://api.twitch.tv/helix/schedule?broadcaster_id=${res.data.data[0].id}`, {
+          headers: {
+            'Authorization': `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
+            'Client-ID': process.env.TWITCH_CLIENT_ID || ''
+          }
+        }).then((resp) => {
+          streams = resp.data.data.segments;
+          if (intervalID) clearInterval(intervalID);
+          streams[0].start_time = moment().format('YYYY-MM-DD HH:mm:ss');
+
+          CheckForStream(streams, twitchuser);
+          intervalID = setInterval(CheckForStream, (60 * 1000), streams, twitchuser);
+        })
       })
     } catch (err) {
-      console.error(err);
-    };
+      console.log(err)
+    }
+
+    setTimeout(RunDaily, (86400 * 1000)) // Runs every 24 hours
   })
-  Run()
-  setInterval(Run, 5 * (60 * 1000));
+
+  const CheckForStream = (async (Streams: Stream[], usertwitch: TwitchUser) => {
+    const date = new Date()
+    let hours = date.getHours()
+    // Streams.forEach(async (stream: Stream) => {
+      let stream = Streams[0]
+      if (stream.is_recurring) {
+        if (stream.canceled_until) {
+          if (moment(stream.canceled_until).isAfter(moment())) {
+            return;
+          }
+        }
+        if (moment(stream.start_time).isAfter(moment())) {
+          return;
+        }
+        if (moment(stream.end_time).isBefore(moment())) {
+          return;
+        }
+      } else {
+        if (moment(stream.start_time).isAfter(moment())) {
+          return;
+        }
+        if (moment(stream.end_time).isBefore(moment())) {
+          return;
+        }
+      }
+      if (stream.id != lastStream.id) {
+        lastStream = stream;
+
+        const result = await profileSchema.find({
+          brawlhalla: true
+        })
+
+
+        // Calculate the stream duration
+        const getStreamDuration = ((daStream: Stream) => {
+          var streamDuration = moment.duration(moment(daStream.end_time).diff(moment(daStream.start_time)));
+          var streamDurationString: any = streamDuration.asHours().toFixed(1);
+          streamDurationString = `**${streamDurationString}** ${streamDurationString > 1 ? "hours" : "hour"}`;
+          return streamDurationString
+        });
+        const calculatePoints = ((daStream: Stream) => {
+          var streamDuration = moment.duration(moment(daStream.end_time).diff(moment(daStream.start_time)));
+          var streamPoints = parseInt(streamDuration.asMinutes().toFixed(1));
+          return streamPoints
+        });
+
+        // Ensure that the user does not recieve two notifications in case the user is subscribed to notifications on multiple servers
+        const uniqueUsers = result.filter((thing, index, self) =>
+          index === self.findIndex((t) => (
+            t.userId === thing.userId
+          ))
+        )
+
+        var greet: string;
+
+        var greetings = [
+          'Greetings',
+          "Howdy!",
+          "Hello there"
+        ]
+        var greetings1 = [
+          "Good Morning",
+          ...greetings
+        ]
+        var greetings2 = [
+          "Good Afternoon",
+          ...greetings
+        ]
+        var greetings3 = [
+          "Good Evening",
+          ...greetings
+        ]
+
+        if (hours < 12) {
+          greet = greetings1[Math.floor(Math.random()*greetings1.length)];;
+        } else if (hours >= 12 && hours <= 17) {
+          greet = greetings2[Math.floor(Math.random()*greetings2.length)];
+        } else if (hours >= 17 && hours <= 24) {
+          greet = greetings3[Math.floor(Math.random()*greetings3.length)];
+        }
+          
+        uniqueUsers.forEach(async (user) => {
+          let guild = client.guilds.cache.find((g) => g.id === user.guildId)
+          if (!guild) return;
+
+          let member = guild.members.cache.find((member) => member.id === user.userId)
+
+          var messages = [
+            `I have the pleasure to inform you that **Brawlhalla** currently is streaming a INSERT_STREAM_LENGTH long \n**${stream.title}**,\nlive on Twitch.`,
+            `Did you know?!? **Brawlhalla** is live! YES. You heard right! Brawlhalla is streaming a INSERT_STREAM_LENGTH long \n**${stream.title}**\nright now on twitch!`,
+            `Hey, just wanted to let you know that **Brawlhalla** is currently streaming a INSERT_STREAM_LENGTH long \n**${stream.title}**.\nHave a good day 😊`,
+            `Hey there fella, just wanna let ya know that **Brawlhalla** is streaming a \n**${stream.title}**.\nHave a nice one.
+            This stream will last INSERT_STREAM_LENGTH`,
+            `G'day mate, just wanna let ya know **Brawlhalla** is live on this twitch thingy with a fresh INSERT_STREAM_LENGTH \n**${stream.title}**.`
+          ]
+
+          var randomMsg = messages[Math.floor(Math.random()*messages.length)]
+            .replaceAll('INSERT_STREAM_LENGTH', `${getStreamDuration(stream)}`);
+
+          const attachment = new MessageAttachment('./img/banner.jpg', 'banner.jpg');
+          
+          let embed = new MessageEmbed()
+            .setTitle(`${greet}, ${member?.user.username}.`)
+            .setURL('https://www.twitch.tv/brawlhalla')
+            .setDescription(`${randomMsg}
+            *You can earn up to* ${calculatePoints(stream)} *points by watching this stream!*
+            
+            Brawlhalla's next stream is a ${getStreamDuration(Streams[1])} **${Streams[1].title}**  ${moment(Streams[1].start_time).calendar()}.`)
+            .setThumbnail(usertwitch.profile_image_url)
+            .setImage('attachment://banner.jpg')
+            .setFooter({ text: `Sincerely, ${client.user?.username}`, iconURL: client.user?.displayAvatarURL() })
+            .setTimestamp()
+
+          if (member?.id != '271288025428918274') member?.send({ embeds: [embed], files: [attachment] })
+        })
+      }
+    // })
+  })
+  
+  RunDaily()
+  // setInterval(RunDaily, (86400 * 1000)); // Runs every 24 hours
 })
