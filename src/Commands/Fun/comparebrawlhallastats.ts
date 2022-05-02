9e96e43b86c4452fd6381a08c556a66aa59861a4
ArvidW <71834553+ArvidWedtstein@users.noreply.github.com>
2022-05-02 11:16:29 +0200

    d

--- src/Commands/Fun/comparebrawlhallastats.ts
+++ src/Commands/Fun/comparebrawlhallastats.ts
@@ -11,35 +11,29 @@ import profileSchema from '../../schemas/profileSchema';
 import axios from 'axios';
 
 export const command: Command = {
-    name: "brawlhallastats",
-    description: "get your brawlhalla stats",
-    details: "get your brawlhalla stats",
-    aliases: ["bwlstats"],
+    name: "comparebrawlhallastats",
+    description: "compare your brawlhalla stats to another user",
+    details: "compare your brawlhalla stats to another user",
+    aliases: ["cmpbwlstats"],
     group: "Fun",
     hidden: false,
     UserPermissions: ["SEND_MESSAGES"],
     ClientPermissions: ["SEND_MESSAGES", "ADD_REACTIONS"],
     ownerOnly: false,
-    examples: ["brawlhallastats"],
+    examples: ["comparebrawlhallastats {steamid}"],
     
     run: async(client, message, args) => {
         const { guild, channel, author, member, mentions, attachments } = message;
         if (!guild) return
         
-        let userId = author.id;
-        let guildId = guild.id;
-        let u = mentions.users.first()
-        if (u) {
-            userId = u.id;
-        }
         
         profileSchema.findOne({
-            userId,
-            guildId
+            userId: author.id,
+            guildId: guild.id
         }).then(async(results) => {
-            if (!results) return temporaryMessage(channel, `${u ? `${u.username} does`: 'You do'} not have a profile. Please create one with -profile`, 50);
-            if (!results.steamId) return temporaryMessage(channel, `${u ? `${u.username} does`: 'You do'} not have a steam id. Please connect your profile to steam with -connectsteam {steamid}`, 50);
-            if (!results.brawlhallaId) return temporaryMessage(channel, `${u ? `${u.username} does`: 'You do'} not have a brawlhalla id. Please connect your profile to brawlhalla with -connectbrawlhalla {steamid}`, 50);
+            if (!results) return temporaryMessage(channel, 'You do not have a profile. Please create one with -profile', 50);
+            if (!results.steamId) return temporaryMessage(channel, 'You do not have a steam id. Please connect your profile to steam with -connectsteam {steamid}', 50);
+            if (!results.brawlhallaId) return temporaryMessage(channel, 'You do not have a brawlhalla id. Please connect your profile to brawlhalla with -connectbrawlhalla {steamid}', 50);
 
             const brawlhallaId = results.brawlhallaId;
             const steamId = results.steamId;
@@ -85,7 +79,7 @@ export const command: Command = {
                     `〔XP: ${toCodeBlock(xp)}〕\n`,
                     `**Legend with most:**`,
                     `〔wins: ${toCodeBlock(sortedLegendsWins[0].legend_name_key)} - ${toCodeBlock(sortedLegendsWins[0].wins)}〕`,
-                    `〔matchtime: ${toCodeBlock(sortedLegendsMatchtime[0].legend_name_key)} - ${toCodeBlock(sortedLegendsMatchtime[0].matchtime / 60)}min〕`,
+                    `〔matchtime: ${toCodeBlock(sortedLegendsMatchtime[0].legend_name_key)} - ${toCodeBlock(sortedLegendsMatchtime[0].matchtime)}〕`,
                     `〔Damage Dealt: ${toCodeBlock(sortedLegendsDamageDealt[0].legend_name_key)} - ${toCodeBlock(sortedLegendsDamageDealt[0].matchtime)}〕`,
                     `〔Games played: ${toCodeBlock(sortedLegendsGames[0].legend_name_key)} - ${toCodeBlock(sortedLegendsGames[0].games)}〕`,
                     `〔level: ${toCodeBlock(sortedLegendsLevel[0].legend_name_key)} - ${toCodeBlock(sortedLegendsLevel[0].level)}〕`,
