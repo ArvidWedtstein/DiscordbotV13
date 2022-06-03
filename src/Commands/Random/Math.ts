b292127c2e531e2a5fa45408cef7d20b772d2e3d
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-06-03 19:02:15 +0200

    METH command incomming

---
+++ src/Commands/Random/Math.ts
@@ -0,0 +1,67 @@
+import { Command } from '../../Interfaces';
+import { Settings } from '../../Functions/settings';
+import * as gradient from 'gradient-string';
+import language from '../../Functions/language';
+import { addCoins, setCoins, getCoins, getColor } from '../../Functions/economy';
+import Discord, { Client, Intents, Constants, Collection, MessageActionRow, MessageButton, MessageEmbed, GuildMember, EmbedFieldData, MessageAttachment } from 'discord.js';
+import temporaryMessage from '../../Functions/temporary-message';
+import { evaluate } from 'mathjs';
+
+
+export const command: Command = {
+    name: "math",
+    description: "Write a math equation and watch it get solved",
+    details: "Write a math equation and watch it get solved",
+    aliases: ["mathematics", "meth"],
+    hidden: false,
+    UserPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "VIEW_CHANNEL"],
+    ClientPermissions: ["SEND_MESSAGES", "ADD_REACTIONS"],
+    ownerOnly: false,
+    examples: ["math"],
+    run: async(client, message, args) => {
+        const { guild, mentions, author, member, channel } = message;
+        
+        if (!guild) return;
+
+        const filter = (m: any) => m.author.id === author.id;
+        const collector = channel.createMessageCollector({ filter, time: 5 * (60*1000) })
+
+        
+        collector.on('collect', m => {
+            if (m.content.includes(`=`)) collector.stop('solve')
+        });
+        
+        collector.on('end', (collected, reason) => {
+            if (reason != 'solve') return;
+            let equationArgs: any = []
+            collected.each(m => m.content.includes('=') ? equationArgs.push(m.content.trim().replace('=', '')) : equationArgs.push(m.content.trim()))
+            equationArgs = equationArgs.join(' ')
+            try {
+                const embed = new MessageEmbed()
+                    .setColor(client.config.botEmbedHex)
+                    .setDescription([
+                        `Your Question 🤡: *${equationArgs}*`,
+                        `Da Solution: **${Number(evaluate(equationArgs)).toPrecision()}**`
+                    ].join('\n'))
+                    .setFooter({ text: `Requested by ${author.tag}`, iconURL: author.displayAvatarURL() })
+                    .setTimestamp()
+
+                channel.send( {embeds: [embed] });
+            } catch (err) {
+                channel.send({ content: `Invalid Equation` })
+            }
+            
+        });
+        
+        const embed = new MessageEmbed()
+            .setColor(client.config.botEmbedHex)
+            .setTitle(`Meth`)
+            .setDescription(`You have 5 minutes to write the equation.`)
+            .setFooter({ text: `Requested by ${author.tag}`, iconURL: author.displayAvatarURL() })
+            .setTimestamp()
+
+        channel.send( {embeds: [embed] });
+    }
+}
+
+
